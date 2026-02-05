835a89672a1667bbb5f2e22dc409b07f21e58c2d
Thatri2015 <thalesmartini18@gmail.com>
2026-02-04 21:55:59 -0300

    Add main function to run chapeco and santa_catarina

---
+++ main.py
@@ -0,0 +1,9 @@
+from chapeco import rodar_chapeco
+from santa_catarina import rodar_santa_catarina
+
+def main():
+    rodar_chapeco()
+    rodar_santa_catarina()
+
+if __name__ == "__main__":
+    main()
