31d92b438db428fdfaf09ba84255af0b0996e588
Thatri2015 <thalesmartini18@gmail.com>
2026-02-04 22:24:10 -0300

    Add triathlon functions to main execution

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from chapeco import rodar_chapeco
 from santa_catarina import rodar_santa_catarina
+from triathlon_sc import rodar_triathlon_sc
+from triathlon_brasil import rodar_triathlon_brasil
 
 def main():
     rodar_chapeco()
     rodar_santa_catarina()
+    rodar_triathlon_sc()
+    rodar_triathlon_brasil()
 
 if __name__ == "__main__":
     main()
